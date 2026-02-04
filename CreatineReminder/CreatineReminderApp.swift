b503a465e3cee53b2869e9e2c896556a0d9cb106
willbtty <willem.battey@gmail.com>
2026-02-03 18:38:44 -0600

    Initial Commit

---
+++ CreatineReminder/CreatineReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatineReminderApp.swift
+//  CreatineReminder
+//
+//  Created by Will Battey on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CreatineReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
